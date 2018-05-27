6674791a830006e14d2ea4159fdbf1d8eb801f97
surender15 <devulapalli.surender@gmail.com>
2018-05-27 15:48:57 +0530

    My Project Setup Completed Successfully

---
+++ shoppingbackend/src/main/java/com/ds/onlineshopping/ShoppingbackendApplication.java
@@ -0,0 +1,12 @@
+package com.ds.onlineshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingbackendApplication.class, args);
+	}
+}
