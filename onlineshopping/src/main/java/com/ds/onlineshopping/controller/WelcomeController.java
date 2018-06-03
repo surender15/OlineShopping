b5ed401c701574646f81ff3c07e88b53ac05507e
surender15 <devulapalli.surender@gmail.com>
2018-06-03 11:48:27 +0530

    Successfully Loaded Static Resources

--- onlineshopping/src/main/java/com/ds/onlineshopping/controller/WelcomeController.java
+++ onlineshopping/src/main/java/com/ds/onlineshopping/controller/WelcomeController.java
@@ -2,6 +2,8 @@ package com.ds.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class WelcomeController {
@@ -9,11 +11,21 @@ public class WelcomeController {
 	
 	private String message = "Hello World";
 
-	@RequestMapping(value= {"/","/home","/index"})
-	public String welcome() 
+	/*@RequestMapping(value= {"/","/home","/index"})
+	public ModelAndView  welcome() 
 	{
-		
-		return "welcome";
+		ModelAndView mv = new ModelAndView("welcome");
+		mv.addObject("message","welcome to the Online shopping");
+		return mv;
+	}*/
+	
+	@RequestMapping("/Test")
+	public ModelAndView test(@RequestParam(value="greeting",required=false) String greeting) {
+		if(greeting == null)
+			greeting="Welcome to the online shopping";
+		ModelAndView mv= new ModelAndView("welcome");
+		mv.addObject("welcome",greeting);
+		return mv;
 	}
 
 }
